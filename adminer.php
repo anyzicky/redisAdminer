3fbfbf76dcb5dad8bbb9b6df2e6fa6d7567f8fcc
Den <any.zicky@gmail.com>
2015-11-02 19:50:44 +0300

    change color scheme script; add possible view key; add href to many key operation's (edit,del,add)

--- adminer.php
+++ adminer.php
@@ -20,6 +20,39 @@ function debug_print(array $var){
 	echo "<pre>".print_r($var)."</pre>";
 }
 
+
+function keyAdd() {
+	$html = "<p><b>Add key</b></p><div claass='add-key'>";
+	$html .= "<form action='' method='POST' >";
+	//$html .= "<p><label for='type'>Type</label></p>";
+
+	$html .= "<p><label for='key'>Key:</label><input type='text' name='newkey'></p>";
+	$html .= "<p><label for='val'>Value:</label><textarea></textarea></p>";
+	$html .= "<p><input type='submit' name='add' value='Add' /></p>";
+	$html .= "</form>";
+	$html .= "</div>";
+
+	return $html;
+}
+
+function keyInfo($key) {
+	global $redis;
+
+	$html = "<p><b>View key</b></p><table>";
+	$html .= "<thead><th>Type</th><th>Size</th><th>Value</th><th>Operations</th></thead><tbody>";
+	if(strlen($key)){
+		$val = $redis->get($key);
+		
+		$html .= "<tr><td>".$val."</td><td>".strlen($val)."</td><td>".$val."</td><td>";
+		//operation
+			$html .= "<a class='operation' href='?type=key&do=edit&key=".$key."'>edit</a>";
+			$html .= "<a class='operation' href='?type=key&do=delete&key=".$key."'>delete</a>";
+		
+		$html .= "</td></tr>";
+	}
+	$html .= "</tbody></table>";
+	return $html;
+}
 /**
  * TODO: set in main php file
  * @return [type] [description]
@@ -43,7 +76,9 @@ function head($title) {
  * @return [type] [description]1
  */
 function footer() {
-	$footer = "<footer id=\"footer\"><div class=\"innertube\"><p>Help buttons</p></div>";
+	$footer = "<footer id=\"footer\"><div class=\"innertube\">";
+	$footer .= "<a href='?type=key&do=add'>[Add key]</a>";
+	$footer .= "</div>";
 	$footer .= "</footer>";
 	echo $footer;
 }
@@ -69,7 +104,7 @@ function leftMenu(array $keys) {
 		$html = "<nav id=\"nav\"><div class=\"innertube\">";
 		$html .= "<h1>Keys</h1><ul>";
 		foreach($keys as $key){
-			$html .= "<li><a href=\"#\">" . $key . "</a></li>";
+			$html .= "<li><a href=\"?type=key&do=view&key=$key\">" . $key . "</a></li>";
 		}
 		$html .= "</ul></div></nav>";	
 
@@ -82,24 +117,29 @@ head('redisAdminer');
 	$keys = $redis->keys('*');
 	contentHeader();
 
-	$content = <<<CONTENT
-				
-		<main>{$who}
-			<div class="innertube">
-				
-				<h1>Heading</h1>
-				<p>table text</p>
-				
-			</div>
-		</main>
+	$contentHead = "<main>{$who}<div class=\"innertube\">";
+	echo $contentHead;
 
+	//page content
+	switch ($_REQUEST['type']) {
+		case 'key':
+			if($_REQUEST['do'] == 'add')
+				echo keyAdd();
+			if($_REQUEST['do'] == 'view')
+				echo keyInfo($_REQUEST['key']);
+			break;
 		
-CONTENT;
-		echo $content;
+		default:
+			# code...
+			break;
+	}
 
-		leftMenu($arKeys);
+	$contentFooter = "</div></main>";
+	echo $contentFooter;
 
-		footer();
+	leftMenu($arKeys);
+
+	footer();
 			
 endhead();
 //html this end
